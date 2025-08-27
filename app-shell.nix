60fec5f9c69e940c4d923bd60267952fac2e31a2
cinereal <cinereal@riseup.net>
2025-08-27 11:27:12 +0200

    Revert "default to local nixpkgs"
    This reverts commit e37f4c4c6c00a9199da9964da7e015af60defa0b.

--- app-shell.nix
+++ app-shell.nix
@@ -5,7 +5,7 @@
 # or
 # nix build --file ./app-shell.nix --argstr apps "APP,APP,..." && ./result
 
-{ nixpkgs ? <nixpkgs>
+{ nixpkgs ? "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz"
 , apps ? null
 , pythonPackages ? null
 , libs ? null
